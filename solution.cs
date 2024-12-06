210429040a8ac5837276bb39b50055b62f55324e
Kooreis <kooreis.official@gmail.com>
2024-12-06 07:36:31 -0600

    Created a WordDictionary class with a constructor that initializes a root TrieNode. Also added an addWord method that adds a word to the Trie.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,16 @@
-class TrieNode {
+class WordDictionary {
     constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
+        this.root = new TrieNode();
+    }
+
+    addWord(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
     }
 }
